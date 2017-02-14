7c8ac8868ce76cd4141feb45d71b259b23f10330
collen <collen@astronomer.io>
2017-02-14 12:48:47 -0500

    Swapped blue tape for mocha

--- test/test.js
+++ test/test.js
@@ -1,12 +1,23 @@
-import test from 'blue-tape';
+import { assert } from 'chai';
 import fs from 'fs';
 import path from 'path';
 import Transform from '../lib/index.js';
 import JSONStream from 'JSONStream';
 
-test('all the pipes', async (t) => {
-    const transformer = new Transform();
-    const filePath = path.join(__dirname, 'util/test_data.json');
-    const stream = fs.createReadStream(filePath, 'utf8').pipe(JSONStream.parse()); 
-    transformer.transformContact(stream);
+describe('Transform', function() {
+    it('all the pipes', async () => {
+        const transformer = new Transform();
+        const filePath = path.join(__dirname, 'util/test_data.json');
+        const stream = fs.createReadStream(filePath, 'utf8').pipe(JSONStream.parse());
+        transformer.transformContact(stream);
+    });
+
+    it('should have data', async () => {
+        const transformer = new Transform();
+        const filePath = path.join(__dirname, 'util/test_data.json');
+        const stream = fs.createReadStream(filePath, 'utf8').pipe(JSONStream.parse());
+        const data = transformer.transformContact(stream);
+        console.log(data);
+    });
 });
+
